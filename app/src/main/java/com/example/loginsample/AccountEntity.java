275437ca9e6667ee3c4551871c86f4951dd4123c
eazurin <eazurin@unsa.edu.pe>
2024-09-24 18:04:52 -0500

    Creacion interfaz grafica

---
+++ app/src/main/java/com/example/loginsample/AccountEntity.java
@@ -0,0 +1,63 @@
+package com.example.loginsample;
+
+public class AccountEntity {
+    private String firstnane;
+
+    private String lastnane;
+
+    private String email;
+
+    private String phone;
+
+    private String usernane;
+
+    private String password;
+
+    public String getFirstnane() {
+        return firstnane;
+    }
+
+    public void setFirstnane(String firstnane) {
+        this.firstnane = firstnane;
+    }
+
+    public String getLastnane() {
+        return lastnane;
+    }
+
+    public void setLastnane(String lastnane) {
+        this.lastnane = lastnane;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsernane() {
+        return usernane;
+    }
+
+    public void setUsernane(String usernane) {
+        this.usernane = usernane;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/com/example/loginsample/LoginActivity.java
+++ app/src/main/java/com/example/loginsample/LoginActivity.java
@@ -9,15 +9,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.loginsample.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     @Override
